d465f29df617beaf111ba5b7c55f3f6eb7d77c4c
rohit khatana <rohitkhatana.khatana@gmail.com>
2019-10-14 01:26:43 +0700

    (WIP) logger needs to be proxied instead of wrapping all method

---
+++ index.js
@@ -0,0 +1,29 @@
+const logger = require('./lib/index');
+const contextMiddleware = require('./lib/context/ContextMiddleware');
+const RestClient = require('./lib/rest_client/RestClient');
+const httpContext = require('express-http-context');
+
+function getConfig() {
+   try {
+      const config = require(process.cwd() + '/config')
+      return config;
+    } catch (e) {
+      console.log('config is missing(required)');
+      throw e;
+    }
+}
+
+function __logger() {
+  return logger(getConfig());
+}
+
+module.exports = {
+
+  logger: () => {
+    return __logger()
+  },
+  contextMiddleware: contextMiddleware,
+  httpContextMiddleware: httpContext.middleware,
+  httpContext: httpContext,
+  restClient: () => RestClient(__logger(), httpContext)
+}
